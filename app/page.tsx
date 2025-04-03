d908308943f25eb6f842b3aa85ad09ac3b2290e8
Thanaphum Laohabunjong <thanaphumlaohabunjong@gmail.com>
2025-04-03 10:29:15 +0700

    update page.tsx for some text

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
  export default function MyApp() {
-  return <div>Hello World!!</div>
+  return( <div>
+    <h1>Hell World!!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente inventore dolorem, deserunt, mollitia fugit cum eveniet vero dolorum expedita corporis saepe nesciunt vitae, veniam a adipisci accusamus architecto illo dolores.
+    </p>
+    </div>
+  )
  }
 
